8f3b897e7d234144dd4f0823878d0a013e1071c3
Laurence Bank <bitbank@pobox.com>
2020-08-19 18:21:47 -0400

    Added Apache 2.0 license

--- main.c
+++ main.c
@@ -5,7 +5,17 @@
 // Play animated GIF files directly onto a framebuffer or SPI LCD
 // Written by Larry Bank
 // bitbank@pobox.com
-// Copyright (c) 2018 BitBank Software, Inc.
+// Copyright (c) 2018 BitBank Software, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===========================================================================
 //
 
 #include <string.h>
--- pil_lzw.c
+++ pil_lzw.c
@@ -1,6 +1,5 @@
 //
 // PIL - portable imaging library
-// Copyright (c) 2000-2018 BitBank Software, Inc.
 // Written by Larry Bank
 // Project started 12/9/2000
 // A highly optimized imaging library designed for resource-constrained
@@ -8,6 +7,18 @@
 //
 // pil_lzw.c - functions related to GIF/LZW compression
 //
+// Copyright 2018 BitBank Software, Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===========================================================================
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
